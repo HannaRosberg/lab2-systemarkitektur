34d16800bea31dfd2d30ba180a42d62a6b408dd3
hannabanana89 <hannaarosberg@gmail.com>
2024-10-26 16:30:50 +0200

    Added the code for GET and POST requests and the links to the localhost requests in sn http file. And added an application in WarehouseApplication. And added a readme file.

---
+++ src/main/java/org/example/WarehouseResource.java
@@ -0,0 +1,79 @@
+package org.example;
+
+import entities.Category;
+import entities.Product;
+import interceptor.Log;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import service.WarehouseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jakarta.validation.Valid;
+import java.util.List;
+
+@Path("/")
+@Log
+public class WarehouseResource {
+    private WarehouseService warehouseService;
+
+    private static final Logger logger = LoggerFactory.getLogger(WarehouseResource.class);
+
+    public WarehouseResource() {
+    }
+
+    @Inject
+    public WarehouseResource(WarehouseService warehouseService) {
+        System.out.println("WarehouseResource created");
+        this.warehouseService = warehouseService;
+    }
+
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hi and welcome to our warehouse, we hope that you will find what you are looking for!";
+    }
+
+    @GET
+    @Path("/products")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllProducts(@QueryParam("page") @DefaultValue("1") int page,
+                                   @QueryParam("size") @DefaultValue("10") int size) {
+        logger.info("Fetching products for page: {} and size: {}", page, size);
+        List<Product> products = warehouseService.getAllProducts(page, size);
+        return Response.ok(products).build();
+    }
+
+    @GET
+    @Path("/products/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProductById(@PathParam("id") String id) {
+        return warehouseService.getProductById(id)
+                .map(product -> Response.ok(product).build())
+                .orElseGet(() -> {
+                    logger.error("Invalid id {}", id);
+                    return Response.status(Response.Status.NOT_FOUND).build();
+                });
+    }
+
+    @GET
+    @Path("/products/categories/{category}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProductsByCategory(@PathParam("category") Category category) {
+        logger.info("Fetching products for category: {}", category);
+        return Response.ok(warehouseService.getProductsByCategory(category))
+                .build();
+    }
+
+    @POST
+    @Path("/products")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addProduct(@Valid Product product) {
+        warehouseService.addProduct(product);
+        logger.info("Product added: {}", product);
+        return Response.status(Response.Status.CREATED).build();
+    }
+}
